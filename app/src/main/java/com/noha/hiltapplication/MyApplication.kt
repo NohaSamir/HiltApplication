1d442eff31b048638ca5155e9d4ed4f6017ddb6e
NohaSamir <noha@savics.org>
2020-06-24 15:59:25 +0200

    Hilt Setup

---
+++ app/src/main/java/com/noha/hiltapplication/MyApplication.kt
@@ -0,0 +1,9 @@
+package com.noha.hiltapplication
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+//ToDo Setup 4: Add @HiltAndroidApp annotation
+@HiltAndroidApp
+class MyApplication : Application() {
+}
