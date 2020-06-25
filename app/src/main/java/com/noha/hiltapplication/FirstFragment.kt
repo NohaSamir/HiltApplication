4a45ceb436ee4c29c8f00cd83f10302c011cbddf
NohaSamir <noha@savics.org>
2020-06-25 03:36:40 +0200

    Field Injection

--- app/src/main/java/com/noha/hiltapplication/FirstFragment.kt
+++ app/src/main/java/com/noha/hiltapplication/FirstFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
-import com.bumptech.glide.RequestManager
 import com.noha.hiltapplication.model.Car
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_first.*
